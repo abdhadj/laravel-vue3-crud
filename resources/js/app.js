c3faed4de9a277f2a53c75f99b7d78e0b32027be
abdhadj <abdhadjabdhadj@gmail.com>
2023-05-22 00:00:09 +0100

    Add router vue + create post

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,22 @@ import './bootstrap';
 // Alpine.start();
 
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router' 
+import App from './layouts/App.vue' 
 import PostsIndex from './components/Posts/Index.vue' 
+import PostsCreate from './components/Posts/Create.vue' 
 
-createApp({}) 
-    .component('PostsIndex', PostsIndex)
+
+const routes = [ 
+    { path: '/', component: PostsIndex },
+    { path: '/posts/create', component: PostsCreate }, 
+] 
+ 
+const router = createRouter({ 
+    history: createWebHistory(),
+    routes
+}) 
+createApp(App) 
+    // .component('PostsIndex', PostsIndex)
+    .use(router) 
     .mount('#app') 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100" id="app">
+    <body class="font-sans antialiased" id="app">
+        <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
